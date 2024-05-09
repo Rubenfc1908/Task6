a67e838d546b6509aee9566004113be7f4a62fff
Rubenfc1908 <94533456+Rubenfc1908@users.noreply.github.com>
2024-05-09 21:51:47 +0200

    Update romans.js

--- romans.js
+++ romans.js
@@ -4,204 +4,204 @@ var INVALID_INTEGER = 'Please enter a valid integer';
 var OUT_OF_RANGE = 'Out of range (1-3999)';
 
 function init() {
-  var modeCheckbox = document.querySelector("input[type='checkbox']");
-  var header = document.querySelector('h1');
-  var convertButton = document.querySelector('.convert-button');
-  var outputArea = document.querySelector('.convert-output');
-  var inputArea = document.querySelector("input[type='text']");
+    var modeCheckbox = document.querySelector("input[type='checkbox']");
+    var header = document.querySelector('h1');
+    var convertButton = document.querySelector('.convert-button');
+    var outputArea = document.querySelector('.convert-output');
+    var inputArea = document.querySelector("input[type='text']");
 
-  modeCheckbox.addEventListener('change', function(e) {
-    header.innerHTML = getModeTitle(e.target.checked);
-  });
+    modeCheckbox.addEventListener('change', function(e) {
+        header.innerHTML = getModeTitle(e.target.checked);
+    });
 
-  function getModeTitle(integerToRoman) {
-    return integerToRoman ? 'Integer To Roman' : 'Roman To Integer';
-  }
-
-  convertButton.addEventListener('click', function() {
-    var inputValue = inputArea.value;
-    var conversion = modeCheckbox.checked ? convertIntegerToRoman(inputValue) : convertRomanToInteger(inputValue);
-    if (conversion.result) {
-      outputArea.innerHTML = conversion.value;
-    } else {
-      alert(conversion.message);
+    function getModeTitle(integerToRoman) {
+        return integerToRoman ? 'Integer To Roman' : 'Roman To Integer';
     }
-  });
+
+    convertButton.addEventListener('click', function() {
+        var inputValue = inputArea.value;
+        var conversion = modeCheckbox.checked ? convertIntegerToRoman(inputValue) : convertRomanToInteger(inputValue);
+        if (conversion.result) {
+            outputArea.innerHTML = conversion.value;
+        } else {
+            alert(conversion.message);
+        }
+    });
 }
 
 function convertRomanToInteger(roman) {
-  var response = {
-    value: 0,
-    message: '',
-    result: false
-  };
+    var response = {
+        value: 0,
+        message: '',
+        result: false
+    };
 
-  var romanNumeralRegex = new RegExp(
-    /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
-  );
+    var romanNumeralRegex = new RegExp(
+        /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
+    );
 
-  roman = roman.toUpperCase();
-  var regexResult = romanNumeralRegex.test(roman);
+    roman = roman.toUpperCase();
+    var regexResult = romanNumeralRegex.test(roman);
 
-  if (!regexResult || roman.length === 0) {
-    response.message = INVALID_ROMAN;
-    return response;
-  }
-
-  var arr = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-  var values = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-  };
-
-  var sum = 0;
-  var prevIndex = 0;
-
-  for (var i = roman.length - 1; i >= 0; i--) {
-    if (arr.indexOf(roman[i]) >= prevIndex) {
-      sum += values[roman[i]];
-    } else {
-      sum -= values[roman[i]];
+    if (!regexResult || roman.length === 0) {
+        response.message = INVALID_ROMAN;
+        return response;
     }
 
-    prevIndex = arr.indexOf(roman[i]);
-  }
+    var arr = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+    var values = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
 
-  response.value = sum;
-  response.result = true;
+    var sum = 0;
+    var prevIndex = 0;
 
-  return response;
+    for (var i = roman.length - 1; i >= 0; i--) {
+        if (arr.indexOf(roman[i]) >= prevIndex) {
+            sum += values[roman[i]];
+        } else {
+            sum -= values[roman[i]];
+        }
+
+        prevIndex = arr.indexOf(roman[i]);
+    }
+
+    response.value = sum;
+    response.result = true;
+
+    return response;
 }
 
 function convertIntegerToRoman(num) {
-  var response = {
-    value: '',
-    message: '',
-    result: false
-  };
+    var response = {
+        value: '',
+        message: '',
+        result: false
+    };
 
-  var numberRegex = new RegExp(/^\d+$/);
-  var regexResult = numberRegex.test(num.toString());
+    var numberRegex = new RegExp(/^\d+$/);
+    var regexResult = numberRegex.test(num.toString());
 
-  if (!regexResult) {
-    response.message = INVALID_INTEGER;
+    if (!regexResult) {
+        response.message = INVALID_INTEGER;
+        return response;
+    }
+
+    if (num > 3999 || num < 1) {
+        response.message = OUT_OF_RANGE;
+        return response;
+    }
+
+    var mapping = {
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M'
+    };
+
+    var count = 1;
+    var str = '';
+    while (num > 0) {
+        var last = parseInt(num % 10) * count;
+        if (last < 10) {
+            str = lessThan9(last, mapping) + str;
+        } else {
+            str = greaterThan9(last, mapping) + str;
+        }
+        count *= 10;
+        num = parseInt(num / 10);
+    }
+
+    response.value = str;
+    response.result = true;
+
     return response;
-  }
+}
 
-  if (num > 3999 || num < 1) {
-    response.message = OUT_OF_RANGE;
-    return response;
-  }
-
-  var mapping = {
-    1: 'I',
-    5: 'V',
-    10: 'X',
-    50: 'L',
-    100: 'C',
-    500: 'D',
-    1000: 'M'
-  };
-
-  var count = 1;
-  var str = '';
-  while (num > 0) {
-    var last = parseInt(num % 10) * count;
-    if (last < 10) {
-      str = lessThan9(last, mapping) + str;
+function lessThan9(num, mapping) {
+    var result = '';
+    if (num === 9) {
+        return mapping[1] + mapping[10];
+    } else if (num >= 5 && num < 9) {
+        result = mapping[5];
+        for (var i = 0; i < num % 5; i++) {
+            result += mapping[1];
+        }
+        return result;
+    } else if (num === 4) {
+        return mapping[1] + mapping[5];
     } else {
-      str = greaterThan9(last, mapping) + str;
+        for (var j = 0; j < num; j++) {
+            result += mapping[1];
+        }
+        return result;
     }
-    count *= 10;
-    num = parseInt(num / 10);
-  }
-
-  response.value = str;
-  response.result = true;
-
-  return response;
 }
 
-function lessThan9(num, obj) {
-  var result = '';
-  if (num === 9) {
-    return obj[1] + obj[10];
-  } else if (num >= 5 && num < 9) {
-    result = obj[5];
-    for (var i = 0; i < num % 5; i++) {
-      result += obj[1];
+function greaterThan9(num, mapping) {
+    var result = '';
+    if (num >= 10 && num < 50) {
+        if (num === 10) {
+            return mapping[10];
+        }
+        if (num === 40) {
+            return mapping[10] + mapping[50];
+        }
+        for (var k = 0; k < parseInt(num / 10); k++) {
+            result += mapping[10];
+        }
+        return result;
+    } else if (num >= 50 && num < 100) {
+        if (num === 50) {
+            return mapping[50];
+        }
+        if (num === 90) {
+            return mapping[10] + mapping[100];
+        }
+        result = mapping[50];
+        for (var l = 0; l < parseInt((num - 50) / 10); l++) {
+            result += mapping[10];
+        }
+        return result;
+    } else if (num >= 100 && num < 500) {
+        if (num === 100) {
+            return mapping[100];
+        }
+        if (num === 400) {
+            return mapping[100] + mapping[500];
+        }
+        for (var m = 0; m < parseInt(num / 100); m++) {
+            result += mapping[100];
+        }
+        return result;
+    } else if (num >= 500 && num < 1000) {
+        if (num === 500) {
+            return mapping[500];
+        }
+        if (num === 900) {
+            return mapping[100] + mapping[1000];
+        }
+        result = mapping[500];
+        for (var n = 0; n < parseInt((num - 500) / 100); n++) {
+            result += mapping[100];
+        }
+        return result;
+    } else if (num >= 1000 && num < 5000) {
+        if (num === 1000) {
+            return mapping[1000];
+        }
+        for (var o = 0; o < parseInt(num / 1000); o++) {
+            result += mapping[1000];
+        }
+        return result;
     }
-    return result;
-  } else if (num === 4) {
-    return obj[1] + obj[5];
-  } else {
-    for (var i = 0; i < num; i++) {
-      result += obj[1];
-    }
-    return result;
-  }
-}
-
-function greaterThan9(num, obj) {
-  var result = '';
-  if (num >= 10 && num < 50) {
-    if (num === 10) {
-      return obj[10];
-    }
-    if (num === 40) {
-      return obj[10] + obj[50];
-    }
-    for (var i = 0; i < parseInt(num / 10); i++) {
-      result += obj[10];
-    }
-    return result;
-  } else if (num >= 50 && num < 100) {
-    if (num === 50) {
-      return obj[50];
-    }
-    if (num === 90) {
-      return obj[10] + obj[100];
-    }
-    result = obj[50];
-    for (var i = 0; i < parseInt((num - 50) / 10); i++) {
-      result += obj[10];
-    }
-    return result;
-  } else if (num >= 100 && num < 500) {
-    if (num === 100) {
-      return obj[100];
-    }
-    if (num === 400) {
-      return obj[100] + obj[500];
-    }
-    for (var i = 0; i < parseInt(num / 100); i++) {
-      result += obj[100];
-    }
-    return result;
-  } else if (num >= 500 && num < 1000) {
-    if (num === 500) {
-      return obj[500];
-    }
-    if (num === 900) {
-      return obj[100] + obj[1000];
-    }
-    result = obj[500];
-    for (var i = 0; i < parseInt((num - 500) / 100); i++) {
-      result += obj[100];
-    }
-    return result;
-  } else if (num >= 1000 && num < 5000) {
-    if (num === 1000) {
-      return obj[1000];
-    }
-    for (var i = 0; i < parseInt(num / 1000); i++) {
-      result += obj[1000];
-    }
-    return result;
-  }
 }
